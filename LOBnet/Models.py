e551d4784f12d226d4b60c5c803d493feb4002fd
Jack-Klingenberg <jackklingenberg1@gmail.com>
2024-12-05 20:41:05 -0500

    Fix V0 model

--- LOBnet/Models.py
+++ LOBnet/Models.py
@@ -4,8 +4,122 @@ import torch.nn.functional as F
 
 # Components for DeepLOB Model (https://arxiv.org/pdf/1808.03668) 
 # Parameters allow some customization while keeping the structure of the DeepLOB paper's model intact
+class DeepLOB_Network_v0(nn.Module):
+    def __init__(self, num_classes=3):
+        super().__init__()
+        self.num_classes = num_classes
+        
+        # Convolutional Feature Extractors
+        self.conv1_layer1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(1, 2), stride=(1, 2))
+        self.conv1_activation1 = nn.LeakyReLU(negative_slope=0.01)
+        self.conv1_bn1 = nn.BatchNorm2d(32)
+        self.conv1_layer2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(4, 1))
+        self.conv1_activation2 = nn.LeakyReLU(negative_slope=0.01)
+        self.conv1_bn2 = nn.BatchNorm2d(32)
+        self.conv1_layer3 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(4, 1))
+        self.conv1_activation3 = nn.LeakyReLU(negative_slope=0.01)
+        self.conv1_bn3 = nn.BatchNorm2d(32)
+
+        self.conv2_layer1 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(1, 2), stride=(1, 2))
+        self.conv2_activation1 = nn.Tanh()
+        self.conv2_bn1 = nn.BatchNorm2d(32)
+        self.conv2_layer2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(4, 1))
+        self.conv2_activation2 = nn.Tanh()
+        self.conv2_bn2 = nn.BatchNorm2d(32)
+        self.conv2_layer3 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(4, 1))
+        self.conv2_activation3 = nn.Tanh()
+        self.conv2_bn3 = nn.BatchNorm2d(32)
+
+        self.conv3_layer1 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(1, 10))
+        self.conv3_activation1 = nn.LeakyReLU(negative_slope=0.01)
+        self.conv3_bn1 = nn.BatchNorm2d(32)
+        self.conv3_layer2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(4, 1))
+        self.conv3_activation2 = nn.LeakyReLU(negative_slope=0.01)
+        self.conv3_bn2 = nn.BatchNorm2d(32)
+        self.conv3_layer3 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(4, 1))
+        self.conv3_activation3 = nn.LeakyReLU(negative_slope=0.01)
+        self.conv3_bn3 = nn.BatchNorm2d(32)
+
+        # Inception Block
+        self.inp1_conv1 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(1, 1), padding='same')
+        self.inp1_activation1 = nn.LeakyReLU(negative_slope=0.01)
+        self.inp1_bn1 = nn.BatchNorm2d(64)
+        self.inp1_conv2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 1), padding='same')
+        self.inp1_activation2 = nn.LeakyReLU(negative_slope=0.01)
+        self.inp1_bn2 = nn.BatchNorm2d(64)
+
+        self.inp2_conv1 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(1, 1), padding='same')
+        self.inp2_activation1 = nn.LeakyReLU(negative_slope=0.01)
+        self.inp2_bn1 = nn.BatchNorm2d(64)
+        self.inp2_conv2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(5, 1), padding='same')
+        self.inp2_activation2 = nn.LeakyReLU(negative_slope=0.01)
+        self.inp2_bn2 = nn.BatchNorm2d(64)
+
+        self.inp3_pool = nn.MaxPool2d((3, 1), stride=(1, 1), padding=(1, 0))
+        self.inp3_conv = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(1, 1), padding='same')
+        self.inp3_activation = nn.LeakyReLU(negative_slope=0.01)
+        self.inp3_bn = nn.BatchNorm2d(64)
+
+        # LSTM and Fully Connected Layers
+        self.lstm = nn.LSTM(input_size=192, hidden_size=64, num_layers=1, batch_first=True)
+        self.fc1 = nn.Linear(64, self.num_classes)
+    
+    def forward(self, input_tensor):
+        hidden_state = torch.zeros(1, input_tensor.size(0), 64).to(input_tensor.device)
+        cell_state = torch.zeros(1, input_tensor.size(0), 64).to(input_tensor.device)
+
+        # Feature Extraction 
+        x = self.conv1_layer1(input_tensor)
+        x = self.conv1_activation1(self.conv1_bn1(x))
+        x = self.conv1_layer2(x)
+        x = self.conv1_activation2(self.conv1_bn2(x))
+        x = self.conv1_layer3(x)
+        x = self.conv1_activation3(self.conv1_bn3(x))
+
+        x = self.conv2_layer1(x)
+        x = self.conv2_activation1(self.conv2_bn1(x))
+        x = self.conv2_layer2(x)
+        x = self.conv2_activation2(self.conv2_bn2(x))
+        x = self.conv2_layer3(x)
+        x = self.conv2_activation3(self.conv2_bn3(x))
+
+        x = self.conv3_layer1(x)
+        x = self.conv3_activation1(self.conv3_bn1(x))
+        x = self.conv3_layer2(x)
+        x = self.conv3_activation2(self.conv3_bn2(x))
+        x = self.conv3_layer3(x)
+        x = self.conv3_activation3(self.conv3_bn3(x))
+
+        # Inception Blocks
+        x_incep1 = self.inp1_conv1(x)
+        x_incep1 = self.inp1_activation1(self.inp1_bn1(x_incep1))
+        x_incep1 = self.inp1_conv2(x_incep1)
+        x_incep1 = self.inp1_activation2(self.inp1_bn2(x_incep1))
+
+        x_incep2 = self.inp2_conv1(x)
+        x_incep2 = self.inp2_activation1(self.inp2_bn1(x_incep2))
+        x_incep2 = self.inp2_conv2(x_incep2)
+        x_incep2 = self.inp2_activation2(self.inp2_bn2(x_incep2))
+
+        x_incep3 = self.inp3_conv(x)
+        x_incep3 = self.inp3_activation(self.inp3_bn(x_incep3))
+        x_incep3 = self.inp3_pool(x_incep3)
+
+        x = torch.cat((x_incep1, x_incep2, x_incep3), dim=1)
+
+        # Permute and reshape to get in right form for LSTM
+        x = x.permute(0, 2, 1, 3)
+        x= x.reshape(-1, x.shape[1], x.shape[2])
+
+        # LSTM and fully connected layers
+        x, _ = self.lstm(x, (hidden_state, cell_state))
+        x = self.fc1(x[:, -1, :])
+        forecast_output = torch.softmax(x, dim=1)
+
+        return forecast_output
+
 class DeepLOB_ConvolutionalBlock(nn.Module):
-    def __init__(self, input_depth=1, output_depth=16, activation=nn.LeakyReLU, slope=.01):
+    def __init__(self, input_depth=1, output_depth=32, activation=nn.LeakyReLU, slope=.01):
         super().__init__()
         self.slope = slope # slope for LeakyReLU
 
@@ -45,7 +159,7 @@ class DeepLOB_ConvolutionalBlock(nn.Module):
         return y
     
 class DeepLOB_InceptionConvSubunit(nn.Module):
-    def __init__(self, input_depth=16, output_depth=32, slope=.01, kernel_size = (3,1)):
+    def __init__(self, input_depth=32, output_depth=64, slope=.01, kernel_size = (3,1)):
         super().__init__()
         self.conv1 = nn.Conv2d(in_channels=input_depth, out_channels=output_depth, kernel_size=(1,1), padding="same")
         self.act1 = nn.LeakyReLU(slope)
@@ -61,7 +175,7 @@ class DeepLOB_InceptionConvSubunit(nn.Module):
         return y
     
 class DeepLOB_IncpetionPoolSubunut(nn.Module):
-    def __init__(self, input_depth=16, output_depth=32, slope=.01):
+    def __init__(self, input_depth=32, output_depth=64, slope=.01):
         super().__init__()
         self.pool1 = nn.MaxPool2d((3,1), stride=(1,1), padding=(1,0))
         self.conv1 = nn.Conv2d(in_channels=input_depth, out_channels=output_depth, kernel_size=(1,1), padding='same')
